f9c47eb30b168022a67c774299d9ed32069e6cc0
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-27 16:40:46 -0300

    directiva input cambiar color

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,15 +1,24 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit {
 
-  constructor() {
-    console.log('constructir directive')
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color: string = 'red';
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el
   }
 
   ngOnInit(): void {
     console.log('NgOnInit en la directiva')
+    this.setColor()
   }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
+  }
+
 }
