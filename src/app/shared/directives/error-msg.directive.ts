171ecf5f9623600938873ca763a9f63e4f139796
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-27 17:08:49 -0300

    cambiar mensaje de etiqueta

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,8 @@ export class ErrorMsgDirective implements OnInit {
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() color: string = 'red';
+  @Input() mensaje: string = '';
+  @Input() modificar: boolean = false;
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
@@ -15,10 +17,20 @@ export class ErrorMsgDirective implements OnInit {
   ngOnInit(): void {
     console.log('NgOnInit en la directiva')
     this.setColor()
+    this.setMensaje()
+    this.setClass()
   }
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color;
   }
 
+  setMensaje(): void {
+    if (this.mensaje === '') return
+    this.htmlElement.nativeElement.innerText = this.mensaje;
+  }
+  setClass(): void {
+    if (!this.modificar) return
+    this.htmlElement.nativeElement.classList.add('form-text')
+  }
 }
