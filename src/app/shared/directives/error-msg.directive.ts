0480752bb251313eb88dc85c681a629396ea964a
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-27 22:24:32 -0300

    setters input

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -4,19 +4,21 @@ import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit {
-  private _color: string = 'red';
+  private _color: string = 'pink';
+  private _mensaje: string = 'Este campo es requerido';
 
   htmlElement: ElementRef<HTMLElement>;
   @Input() set color(valor: string) {
-    this.htmlElement.nativeElement.style.color = valor;
     this._color = valor;
+    this.setColor()
   };
   // @Input() mensaje: string = '';
-  @Input() modificar: boolean = false;
   @Input() set mensaje(valor: string) {
-    this.htmlElement.nativeElement.innerText = valor
+    this._mensaje = valor;
+    this.setMensaje()
   }
 
+  @Input() modificar: boolean = false;
 
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -26,8 +28,9 @@ export class ErrorMsgDirective implements OnInit {
 
   ngOnInit(): void {
     console.log('NgOnInit en la directiva')
-    // this.setColor()
-    // this.setMensaje()
+
+    this.setColor()
+    this.setMensaje()
     this.setClass()
   }
 
@@ -37,14 +40,14 @@ export class ErrorMsgDirective implements OnInit {
   // }
 
 
-  // setColor(): void {
-  //   this.htmlElement.nativeElement.style.color = this.color;
-  // }
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
 
-  // setMensaje(): void {
-  //   if (this.mensaje === '') return
-  //   this.htmlElement.nativeElement.innerText = this.mensaje;
-  // }
+  setMensaje(): void {
+    if (this.mensaje === '') return
+    this.htmlElement.nativeElement.innerText = this._mensaje;
+  }
   setClass(): void {
     if (!this.modificar) return
     this.htmlElement.nativeElement.classList.add('form-text')
